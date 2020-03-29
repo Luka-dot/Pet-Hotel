2f28db8b5417f57079d34b0acb523fb004a2a6bd
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-03-28 20:45:15 -0400

    added booking

--- app.js
+++ app.js
@@ -1,18 +1,85 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql'); //basically middleware before express
-const { buildSchema }= require('graphql');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+
+const Booking = require('./models/booking');
 
 const app = express();
 
-
-// parsing upcoming json bodies
 app.use(bodyParser.json());
-// inside graphqlHttp() is object {} there we configure graphql api
-app.use('/graphql', graphqlHttp({ 
-    schema: null,     // this needs to point to graphQl schema
-    rootValue: {}       // points to {} with all the resolves function and match schema with names
-}))
 
-app.listen(3000);
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+        type Booking {
+          _id: ID!
+          customer: String!
+          startDate: String!
+          endDate: String!
+          price: Float!
+          date: String!
+        }
+        input BookingInput {
+            customer: String!
+            startDate: String!
+            endDate: String!
+            price: Float!
+            date: String!
+        }
+        type RootQuery {
+            bookings: [Booking!]!
+        }
+        type RootMutation {
+            createBooking(bookingInput: BookingInput): Booking
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      bookings: () => {
+        return Booking.find()
+          .then(bookings => {
+            return bookings.map(booking => {
+              return { ...booking._doc, _id: booking.id };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createBooking: args => {
+        const booking = new Booking({
+          customer: args.bookingInput.customer,
+          startDate: new Date(args.bookingInput.startDate),
+          endDate: new Date(args.bookingInput.endDate),
+          price: +args.bookingInput.price,
+          date: new Date(args.bookingInput.date)
+        });
+        return booking
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      }
+    },
+    graphiql: true
+  })
+);
+
+// nodemon.json is set up with username and password. passing those variables to connection to mongoose and starting server
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-9lijc.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,{useUnifiedTopology: true}).then(() => {
+                    app.listen(3000);;
+                    }).catch(err => { 
+                        console.log('logging error in connect ',err);
+                    });
+
---
+++ models/booking.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema({
+  customer: {
+    type: String,
+    required: true
+  },
+  startDate: {
+    type: Date,
+    required: true
+  },
+  endDate: {
+    type: Date,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  }
+  
+});
+
+module.exports = mongoose.model('Booking', bookingSchema);
