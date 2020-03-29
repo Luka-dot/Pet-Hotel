99fe08446a32e78d71d2c3f0cafb625efec70e72
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-03-29 09:46:54 -0400

    createUser iss working with hashed password

--- app.js
+++ app.js
@@ -86,21 +86,32 @@ app.use(
             console.log(err);
             throw err;
           });
-      }
+      
     },
 
-    createUser: async args => {
-        const hashedPassword = await bcrypt.hash(args.userInput.password, 12)
-            const user = new User({
-                email: args.userInput.email,
-                password: hashedPassword
-            });
-            const result = await user.save();
-            // for each event ran: _doc is a property provided by Mongoose. gives {} all properties without metadata
-            return {
-                ...result._doc
+    createUser: args => {
+        return User.findOne({ email: args.userInput.email })
+          .then(user => {
+            if (user) {
+              throw new Error('User exists already.');
             }
-        }, 
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then(hashedPassword => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword
+            });
+            return user.save();
+          })
+          .then(result => {
+            return { ...result._doc, password: null, _id: result.id };
+          })
+          .catch(err => {
+            throw err;
+          });
+      }
+    },
     
 
     graphiql: true
