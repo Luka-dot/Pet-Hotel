d46e9c84c94b43ea3bfb7ccd50ad5dc8721c1a11
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-03-29 13:59:49 -0400

    cancelBooking further testing passing now

--- models/user.js
+++ models/user.js
@@ -12,8 +12,9 @@ const userSchema = new Schema({
     required: true
   },
   createdBookings: [
-    {
+    { // ObjectID is special schema provided by mongodb
       type: Schema.Types.ObjectId,
+      // ref helps set up connection between models. passing name of the model to connect to this case 'Booking'
       ref: 'Booking'
     }
   ]
