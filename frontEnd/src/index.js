57caaf7ef7040dd48855adfe241451ba72ffcf14
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-03-30 19:25:22 -0400

    landing page

--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
@@ -9,9 +10,12 @@ import { Provider } from 'react-redux';
 const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
+  <BrowserRouter>
+  <Route path="/" component={null} />
   <Provider store={store}>
     <App />
-  </Provider>,
+  </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
