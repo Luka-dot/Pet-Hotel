f9d06dc131d2c5da82048e8ce78b96c535d01ec0
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-03-31 22:09:55 -0400

    reverted back to React without Redux fixed navbar

--- frontEnd/src/pages/booking.js
+++ frontEnd/src/pages/booking.js
@@ -9,6 +9,7 @@ class Booking extends Component {
             <div >
                 <h1> BOOKING PAGE </h1>
                 <h1>Placeholder text</h1>
+                <h3>www</h3>
             </div>
         );
     }
