5e02d1c86b0a70eafd6cdf63549cfa59579ebcc5
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-04-03 23:01:50 -0400

    styling and css

--- frontEnd/src/components/bookings/bookingItem.js
+++ frontEnd/src/components/bookings/bookingItem.js
@@ -8,12 +8,12 @@ const bookingItem = props => (
 
         <div className="divTableBody">
             <div className="divTableRow">
-                <h4 className="divTableCell">customer:&nbsp; {props.customer}</h4>
-                <h4 className="divTableCell">pet name: &nbsp;{props.petName}</h4>
+                <h3 className="divTableCell"><a className="inner-text">customer:</a>&nbsp; {props.customer}</h3>
+                <h3 className="divTableCell"><a className="inner-text">pet name:</a> &nbsp;{props.petName}</h3>
             </div>
             <div className="divTableRow">
-                <h4 className="divTableCell">per night rate:&nbsp; {props.price}</h4>
-                <h4 className="divTableCell">this guest is:&nbsp; {props.petType}</h4>
+                <h3 className="divTableCell"><a className="inner-text">per night rate:</a>&nbsp; {props.price}&nbsp;$</h3>
+                <h3 className="divTableCell"><a className="inner-text">this guest is:</a>&nbsp; {props.petType}</h3>
             </div>
             <div className="divTableRow2">
                 <h5 className="divTableCell">check-in:&nbsp; {props.checkIn}</h5>
--- frontEnd/src/components/render/renderItem.js
+++ frontEnd/src/components/render/renderItem.js
@@ -5,7 +5,32 @@ import './render.css';
 // props are passed from bookingList
 const renderItem = props => (
 
-  <li key={props.key} className="divTableS"> 
+  <li key={props.key} className={props.petWeight}> 
+   
+    
+    <div className="divTableRowS">
+    
+    <h4 className="divTableCellS">pet name: &nbsp;{props.petName}</h4>
+</div>
+<div className="divTableRow2">
+    <h4 className="divTableCellS">customer:&nbsp; {props.customer}</h4>
+    <h4 className="divTableCellS">per night rate:&nbsp; {props.price}</h4>
+    <h4 className="divTableCellS">this guest is:&nbsp; {props.petType}</h4>
+</div>
+<div className="divTableRow2">
+    <h5 className="divTableCellS">check-in:&nbsp; {props.checkIn}</h5>
+    <h5 className="divTableCellS">check-out:&nbsp; {props.checkOut}</h5>
+</div>
+
+    
+</li>
+    
+);
+
+export default renderItem;
+
+/*
+<li key={props.key} className="divTableS"> 
    
     <div className="divTableBodyS">
     <div className="divTableRowS">
@@ -24,7 +49,5 @@ const renderItem = props => (
 </div>
     
 </li>
-    
-);
 
-export default renderItem;
+*/
