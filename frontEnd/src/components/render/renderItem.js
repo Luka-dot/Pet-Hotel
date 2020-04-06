e4eda60674c921dd65e4ee8bf996d68415cfde71
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-04-05 22:49:25 -0400

    various fixes and set-up render added

--- frontEnd/src/components/render/renderItem.js
+++ frontEnd/src/components/render/renderItem.js
@@ -7,7 +7,6 @@ const renderItem = props => (
 
     <li key={props.key} className={props.petWeight}>
 
-
         <div className="divTableRowS">
 
             <h4 className="divTableCellS">Pet: &nbsp;{props.petName}</h4>
@@ -21,33 +20,8 @@ const renderItem = props => (
             <h5 className="divTableCellS">check-in:&nbsp; {props.checkIn}</h5>
             <h5 className="divTableCellS">check-out:&nbsp; {props.checkOut}</h5>
         </div>
-
-
     </li>
 
 );
 
 export default renderItem;
-
-/*
-<li key={props.key} className="divTableS">
-
-    <div className="divTableBodyS">
-    <div className="divTableRowS">
-
-    <h4 className="divTableCellS">pet name: &nbsp;{props.petName}</h4>
-</div>
-<div className="divTableRow2">
-    <h4 className="divTableCellS">customer:&nbsp; {props.customer}</h4>
-    <h4 className="divTableCellS">per night rate:&nbsp; {props.price}</h4>
-    <h4 className="divTableCellS">this guest is:&nbsp; {props.petType}</h4>
-</div>
-<div className="divTableRow2">
-    <h5 className="divTableCellS">check-in:&nbsp; {props.checkIn}</h5>
-    <h5 className="divTableCellS">check-out:&nbsp; {props.checkOut}</h5>
-</div>
-</div>
-
-</li>
-
-*/
